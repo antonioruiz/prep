968a15c0a70064b45f33bd99cc6842650ba8af75
anapaulinaf <antonioruiz2000@yahoo.com>
2017-08-08 20:17:04 -0500

    DavisStairs

---
+++ src/main/java/com/aruiz/java8core/DavisStairs.java
@@ -0,0 +1,64 @@
+package com.aruiz.java8core;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+public class DavisStairs {
+
+	// Consumer<>
+
+	public static int findPossibleways(int n) {
+
+		if (n == 1 || n == 2) {
+			return n;
+		}
+		if (n == 3) {
+			return 4;
+		}
+
+		int possibleSteps = 0;
+		Map<Integer, Integer> components = breakIntoLargestPossibleComponents(n);
+		// Stream components = Stream.of(remainder);
+		System.out.println(components);
+		// components.forEach(System.out.println);
+		possibleSteps = calculateResultsAndDisplay(components);
+
+		return possibleSteps;
+
+	}
+
+	private static int calculateResultsAndDisplay(Map<Integer, Integer> components) {
+		int possibleSteps =0;
+		for (Entry<Integer, Integer> entry : components.entrySet()) {
+			System.out.printf("Calculating %d * the value of %d", entry.getKey(), entry.getValue());
+			System.out.println();
+			Integer tempResult = entry.getValue() * (findPossibleways(entry.getKey()));
+			possibleSteps += tempResult;
+		}
+		return possibleSteps;
+	}
+
+	static Map<Integer,Integer> breakIntoLargestPossibleComponents(int n) {
+		// TODO Auto-generated method stub
+		int threes = n / 3;
+		int twos = (n % 3) / 2;
+		int ones = n - (3 * threes + 2 * twos);
+		// System.out.printf("[%d ,%d,%d]",threes, twos, ones);
+		Map<Integer, Integer> rep = new TreeMap<Integer, Integer>();
+
+		rep.put(3, threes);
+		rep.put(2, twos);
+		rep.put(1, ones);
+		return rep;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("3: " + findPossibleways(3));
+		//System.out.println("4: " + findPossibleways(4));
+		System.out.println("5: " + findPossibleways(5));
+		//System.out.println("6:" + findPossibleways(6));
+	}
+
+}
---
+++ src/test/java/com/aruiz/java8core/DavisStairsTest.java
@@ -0,0 +1,49 @@
+package com.aruiz.java8core;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DavisStairsTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFindPossibleways() {
+		assertEquals(1,DavisStairs.findPossibleways(1));
+		assertEquals(2,DavisStairs.findPossibleways(2));
+		assertEquals(4,DavisStairs.findPossibleways(3));
+		//assertEquals(7,DavisStairs.findPossibleways(4));
+		//assertEquals(13, DavisStairs.findPossibleways(5));
+	}
+	
+	@Test
+	public void breakIntoLargestPossibleComponents(){
+		Map<Integer, Integer> calculated = DavisStairs.breakIntoLargestPossibleComponents(5);
+		//break 5 into (3,2), that is 1 three, 1 Two a 0 ones
+		assertEquals(3,calculated.size());
+		assertEquals(1, calculated.get(3).intValue());
+		assertEquals(1, calculated.get(2).intValue());
+		assertEquals(0, calculated.get(1).intValue());
+		assertEquals(new Integer(1),calculated.get(3));
+		assertEquals(5, 3*calculated.get(3) 
+				+ 2*calculated.get(2).intValue()
+				+ 1*calculated.get(1).intValue());
+		
+		calculated = DavisStairs.breakIntoLargestPossibleComponents(7);
+		//break 7 into (3,3,1), that is 1 three, 1 Two a 0 ones
+		assertEquals(3,calculated.size());
+		assertEquals(calculated.get(3).intValue(), 2);
+		assertEquals(calculated.get(2).intValue(), 0);
+		assertEquals(calculated.get(1).intValue(), 1);
+		assertEquals(7, 3*calculated.get(3) 
+				+ 2*calculated.get(2).intValue()
+				+ 1*calculated.get(1).intValue());
+
+	}
+}
